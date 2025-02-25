42f95ebf31cc644cbe90716209344926a72c5f10
Shiv Vyas <shivvyas0209@gmail.com>
2025-02-25 13:15:37 -0500

    chore(Domain Issue) Domain Issue fix 1

--- next.config.js
+++ next.config.js
@@ -2,23 +2,17 @@
 const nextConfig = {
   async redirects() {
     return [
-      // Redirect HTTP to HTTPS
       {
         source: '/:path*',
         has: [
           {
             type: 'host',
-            value: 'shivvyas.com',
+            value: 'www.shivvyas.com',
           },
         ],
         destination: 'https://shivvyas.com/:path*',
         permanent: true,
-      },
-      // Redirect www to non-www
-      {
-        source: '/:path*',
-        has: [
-          {
+      }
             type: 'host',
             value: 'www.shivvyas.com',
           },
