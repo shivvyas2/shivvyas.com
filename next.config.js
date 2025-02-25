fb7744fadddd4d124455dcf5348d051885c2f4f0
Shiv Vyas <shivvyas0209@gmail.com>
2025-02-25 14:14:31 -0500

    Update configuration to use www as main domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     domains: ['shivvyas.com', 'www.shivvyas.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'shivvyas.com',
+          },
+        ],
+        destination: 'https://www.shivvyas.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
